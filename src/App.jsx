b9a45fb3d8be7dfb2c05858d45c0edad1a7666f9
ryokf <ryokhrisnaf@gmail.com>
2024-10-19 08:47:29 +0700

    ✨feature: pertemuan 4 form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">Hello world</h1>
-
+    <LoginPage></LoginPage>
   )
 }
 
