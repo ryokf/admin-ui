8403fa391af665fb2d70a4274a08f5cd1ccb20a8
ryokf <ryokhrisnaf@gmail.com>
2024-10-29 17:59:00 +0700

    🐞 fix:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import ForgetPassword from "./pages/ForgetPassword";
 import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
-import ForgetPasswordForm from "./components/fragments/ForgetPasswordForm";
 import ForgetPassword from "./pages/ForgetPassword";
 
 const App = () => {
